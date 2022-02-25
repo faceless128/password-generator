db74db59e73e5f46f48990775862dc8f15edbd39
Kaijam Jeffers <faceless128@gmail.com>
2022-02-24 22:35:24 -0500

    ask for password length

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Generate random number
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
+
+
+// Check password length
+var passwordLength = function() {
+  var howLong = window.prompt("How long would you like your password to be? Please enter a number between 8 and 128.");
+  howLong = parseInt(howLong);
+  if (howLong >=8 && howLong <=128) {
+    console.log(howLong)
+  } else {
+    window.alert("Please create a password between 8 and 128 characters in length.")
+    passwordLength();
+  }
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +33,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+passwordLength();
